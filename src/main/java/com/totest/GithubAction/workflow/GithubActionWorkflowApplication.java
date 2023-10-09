9d71b12c453c1227db42ce6d4e05619604f55ecb
mittalyash394 <mittalyash394@gmail.com>
2023-10-09 17:13:06 +0530

    added the action workflow project

---
+++ src/main/java/com/totest/GithubAction/workflow/GithubActionWorkflowApplication.java
@@ -0,0 +1,13 @@
+package com.totest.GithubAction.workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionWorkflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionWorkflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/totest/GithubAction/workflow/GithubActionWorkflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.totest.GithubAction.workflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionWorkflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
